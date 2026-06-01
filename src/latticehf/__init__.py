a55d61ceeeaa981047d563ea8112c3b91d1456f6
Echoxiangmega <130957410+Echoxiangmega@users.noreply.github.com>
2026-06-01 11:18:36 +0800

    Export Green-function and order-parameter helpers

--- src/latticehf/__init__.py
+++ src/latticehf/__init__.py
@@ -4,6 +4,26 @@ from .model import LatticeModel
 from .hamiltonian import Hamiltonian, KSpaceHamiltonian
 from .solver import HFSolver, KSpaceHFSolver
 from .quantities import Quantities
+from .green import (
+    density_matrix_from_eigensystem,
+    fermi_function,
+    finite_temperature_density_matrix,
+    green_density_residual,
+    hartree_fock_self_energy,
+    matsubara_green,
+    zero_temperature_density_matrix,
+)
+from .order_parameters import (
+    bond_current,
+    bond_currents,
+    bond_expectation,
+    bond_order,
+    charge_density,
+    charge_order_amplitude,
+    charge_structure_factor,
+    loop_current,
+    loop_current_pattern,
+)
 
 __version__ = "0.1.0"
 
@@ -14,4 +34,20 @@ __all__ = [
     "HFSolver",
     "KSpaceHFSolver",
     "Quantities",
+    "density_matrix_from_eigensystem",
+    "fermi_function",
+    "finite_temperature_density_matrix",
+    "green_density_residual",
+    "hartree_fock_self_energy",
+    "matsubara_green",
+    "zero_temperature_density_matrix",
+    "bond_current",
+    "bond_currents",
+    "bond_expectation",
+    "bond_order",
+    "charge_density",
+    "charge_order_amplitude",
+    "charge_structure_factor",
+    "loop_current",
+    "loop_current_pattern",
 ]
